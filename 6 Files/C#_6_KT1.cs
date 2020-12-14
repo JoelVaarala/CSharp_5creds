a5485bfd140f1d633f2bbb0533b6a6f3021cd7ca
JoelVaarala <bgh275@myy.haaga-helia.fi>
2020-12-15 00:07:24 +0200

    cp 6

---
+++ 6 Files/C#_6_KT1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektiCsharp
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // Konsoli tyylisäätöä
+            Console.Title = "6-1";
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Hakemistot\n");
+
+            DirectoryInfo drInfo = new DirectoryInfo("c:\\temp");
+
+            if (drInfo.Exists != true)
+            {
+                drInfo.Create();
+            }
+
+            // Luodaan alihakemistot
+            for (int i = 1; i<=10; i++) 
+            {
+                drInfo.CreateSubdirectory("alihakemisto"+i);
+            }
+
+            // Lisätään hakemistot taulukkoon
+            string[] hakemistot = Directory.GetDirectories("c:\\temp");
+
+            // Testaus print onko hakemistot taulukossa --- miksiköhän hakemistot[1] tulostaa "alihakemisto10" & index 9 tulostaa alihakemisto9 ??
+            // Console.WriteLine(hakemistot[0].ToString()+ "\n ... \n" +hakemistot[1].ToString());
+
+            foreach (string h in hakemistot)
+            {
+                Console.WriteLine(h);
+            }
+
+            //Console.WriteLine(drInfo.FullName);
+
+            
+            for(int i = 1; i<11; i++)
+            {
+                DirectoryInfo dr = new DirectoryInfo("c:\\temp\\alihakemisto"+i);
+                dr.Delete(true);
+            }
+
+        }
+    }
+}
---
+++ 6 Files/C#_6_KT2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektiCsharp
+{
+    class Program
+    {
+
+        static void ArvoJaTallennaTiedostoon()
+        {
+            double min = 1.4;
+            double max = 5.8;
+            Random rn = new Random();
+
+            String tiedosto = "c:\\temp\\datat.txt";
+            StreamWriter kirj = new StreamWriter(tiedosto, true);
+
+            for (int i = 1; i<=40; i++)
+            {
+                var next = rn.NextDouble();
+                double luku = min + (next * (max - min));
+                double num = Math.Round((double)luku, 2);
+                //Console.WriteLine(luku + "  " + num);
+                kirj.WriteLine(num);
+            }
+            kirj.Close();
+            LueTiedostosta();
+        }
+
+        static void LueTiedostosta()
+        {
+            double[] luvut = new double[40];
+            int i = 0;
+            String tiedosto = "c:\\temp\\datat.txt";
+            StreamReader luk = new StreamReader(tiedosto);
+            while (luk.EndOfStream == false)
+            {
+                luvut[i] = double.Parse(luk.ReadLine());
+                i++;
+                if (i == 39)
+                    break;
+            }
+            luk.Close();
+
+           
+            TulostaTiedot(luvut);
+        }
+
+        static void TulostaTiedot(double[] luvut)
+        {
+
+            double avg, sum = 0;
+
+            for (int j = 0; j <= luvut.Length - 1; j++)
+            {
+                double x = luvut[j];
+                sum += x;
+            }
+
+            avg = sum / luvut.Length;
+            Array.Sort(luvut);
+
+            Console.WriteLine("\nSumma : " + sum + "\nKeskiarvo : " + avg + "\nSuurin : " + luvut[39] + "\nPienin : " + luvut[1] + "\n");
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Konsoli tyylisäätöä
+            Console.Title = "6-2";
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("File_s\n");
+
+            ArvoJaTallennaTiedostoon();
+             
+        }
+    }
+}
